8ac20f228cc2534455d73a62c1f9c58c463b7c95
Ana <anatanweinee@gmail.com>
2022-07-25 20:39:22 +0800

    list items and checkout done lol
    why i do so slow lmao can finish the day the assignment was given wtf

--- miniProject1.py
+++ miniProject1.py
@@ -1,24 +1,20 @@
 #define the list of items
 dairy = {"Milk": 2.30,"Butter": 4.50, "Eggs": 3.40, "Cheese Slices": 3.15,
          "Evaporated Milk Creamer": 1.40, "Milo": 12.50, "Biscuits": 5.30, "Yogurt": 0.95}
-
 packaged_goods = {"Bread": 2.70, "Cereal ": 7.00, "Crackers": 3.10, "Chips": 2.60,
                   "Raisin": 2.10, "Nuts": 2.00, "Green Bean": 1.05, "Barley": 1.05}
-
 canned_goods = {"Tomato": 1.45, "Button Mushroom": 1.15, "Baking Bean": 1.35, "Tuna Fish": 1.45,
                 "Kernel Corn": 1.25, "Sardine Fish": 1.10, "Chicken Luncheon Meat": 1.95, "Pickled Lettuce": 0.95}
-
 condiments_sauces = {"Fine Salt": 0.80, "Sea Salt Flakes": 1.30, "Chicken Stock": 3.15, "Chili Sauce": 2.65,
                      "Oyster Sauce": 4.50, "Sweet Soy Sauce": 3.75, "Tomato Ketchup": 3.20, "Sesame Oil": 4.95}
-
-drinks_and_beverages = {"Green Tea Canned 330ML (33)": 15.00, "Blackcurrent Ribena 330ML (34)": 31.00, "100 Plus 24 Cans (35)": 15.00,
-                        "Orange Cordial 2 Litre (36)": 3.90, "Mineral Water 24 x 600ML (37)": 7.00, "Pineapple Juice (38)": 0.80, "Nescafe Coffee (39)": 9.90, "Coke 24 cans (40)": 12.40}
-
+drinks_and_beverages = {"Green Tea Canned 330ML": 15.00, "Blackcurrent Ribena 330ML": 31.00, "100 Plus 24 Cans": 15.00,
+                        "Orange Cordial 2 Litre": 3.90, "Mineral Water 24 x 600ML": 7.00, "Pineapple Juice": 0.80, "Nescafe Coffee": 9.90, "Coke 24 cans": 12.40}
 shopping_menu= dairy|packaged_goods|canned_goods|condiments_sauces|drinks_and_beverages
 
 #list to show the user data
-total_price=[]
-user_shopping_list=[]
+user_shopping_list_dict={}
+#boolean to check for discount
+discount = False
 
 print("#########################")
 print("##### SHOPPING LIST #####")
@@ -33,9 +29,10 @@ def main():
           "4. View your shopping cart" '\n' 
           "5. Go to checkout" '\n' 
           "6. Exit\n")
-
+    #get input
     action = int(input("Please input the action number: "))
 
+    #check input and runs corresponding functions
     while True:
         #view the shopping list
         if action == 1:
@@ -60,9 +57,6 @@ def main():
             print('Invalid number, please try again')
             main()
 
-
-
-
 #ask for user input for how the data is shown
 def action1():
     print("Do you want to display shopping list according to category, alphabetical order or in ascending price?")
@@ -72,7 +66,6 @@ def action1():
         main()
     else:
         main()
-
 
 #print out items based on category
 def action1_category():
@@ -123,10 +116,11 @@ def action2():
         num=input('Key the number of the item you would like to get [item (no.)]: ')
         global q
         q=int(input('Key the number of the items you would like: '))
-        user_shopping_list.append('{} x {}'.format(q,num))
+        user_shopping_list_dict.append('{} x {}'.format(q,num))
         total_price.append(q*shopping_menu.get(num))
         choice=input('Would you like to add more things to your cart?(y/n) ')
         if choice == 'y':
+            action4()
             continue
         elif choice == 'n':
             main()
@@ -142,11 +136,12 @@ def action3():
         print('\nBe warned that all quantities of the item would be removed\n')
         delete=int(input('Key in the number that you would like to remove'
                          '(1 being the first item in the list)  : '))
-        user_shopping_list.remove(user_shopping_list[delete-1])
+        user_shopping_list_dict.remove(user_shopping_list_dict[delete-1])
         total_price.remove(total_price[delete-1])
         choice=input('Would you like to delete more things from your cart?(y/n) ')
         if choice == 'y':
-            print(user_shopping_list)
+            action4()
+            #print(user_shopping_list_dict)
             continue
         else:
             main()
@@ -154,13 +149,43 @@ def action3():
 
 #View Shopping cart
 def action4():
-    print(user_shopping_list)
+    print("Price without GST\n")
+    #print(user_shopping_list_dict)
+    for key, value in user_shopping_list_dict.items():
+        print(key, "price: "+ str(value) )
+    main()
 
-    # for index in range(len(user_shopping_list)):
-    #     print(user_shopping_list[index], "price", )
 #Go to checkout
 def action5():
-    print("action 5")
+
+    discountCheck = input("Do you have a discount y/n")
+
+    if(discountCheck == "y"):
+        discount = True
+    else:
+        discount = False
+    totalCost = 0
+
+    #calculate total cost before GST and discount
+    for key, value in user_shopping_list_dict.items():
+        totalCost = totalCost + value
+
+    #cost of item with GST
+    print("\nprice with GST")
+    for key, value in user_shopping_list_dict.items():
+        print(key, " price:"+ str(value *1.07) )
+
+    #print total cost
+    print("\ntotal cost including GST:" + str(totalCost*1.07))
+
+    #total discount
+    if (discount == True):
+        print("\nDiscount amount:" + str(totalCost*.1))
+
+    #total GST
+    print("\nGST amount:" + str(totalCost * 0.07))
+
+    main()
 
 #function to return back to main screen
 def exitToMain():
@@ -169,139 +194,3 @@ def exitToMain():
 
 #call main in order to access the rest of the codes
 main()
-
-
-
-
-
-
-
-#commented out
-#
-# main()
-#
-# dairy = {"Milk (1)": 2.30,"Butter (2)": 4.50, "Eggs (3)": 3.40, "Cheese Slices (4)": 3.15,
-#          "Evaporated Milk Creamer (5)": 1.40, "Milo (6)": 12.50, "Biscuits (7)": 5.30, "Yogurt (8)": 0.95}
-#
-# packaged_goods = {"Bread (9)": 2.70, "Cereal (10)": 7.00, "Crackers (11)": 3.10, "Chips (12)": 2.60,
-#                   "Raisin (13)": 2.10, "Nuts (14)": 2.00, "Green Bean (15)": 1.05, "Barley (16)": 1.05}
-#
-# canned_goods = {"Tomato (17)": 1.45, "Button Mushroom (18)": 1.15, "Baking Bean (19)": 1.35, "Tuna Fish (20)": 1.45,
-#                 "Kernel Corn (21)": 1.25, "Sardine Fish (22)": 1.10, "Chicken Luncheon Meat (23)": 1.95, "Pickled Lettuce (24)": 0.95}
-#
-# condiments_sauces = {"Fine Salt (25)": 0.80, "Sea Salt Flakes (26)": 1.30, "Chicken Stock (27)": 3.15, "Chili Sauce (28)": 2.65,
-#                      "Oyster Sauce (29)": 4.50, "Sweet Soy Sauce (30)": 3.75, "Tomato Ketchup (31)": 3.20, "Sesame Oil (32)": 4.95}
-#
-# drinks_and_beverages = {"Green Tea Canned 330ML (33)": 15.00, "Blackcurrent Ribena 330ML (34)": 31.00, "100 Plus 24 Cans (35)": 15.00,
-#                         "Orange Cordial 2 Litre (36)": 3.90, "Mineral Water 24 x 600ML (37)": 7.00, "Pineapple Juice (38)": 0.80, "Nescafe Coffee (39)": 9.90, "Coke 24 cans (40)": 12.40}
-#
-# shopping_menu= dairy|packaged_goods|canned_goods|condiments_sauces|drinks_and_beverages
-
-# print()
-#
-# print("#########################")
-# print("##### SHOPPING LIST #####")
-# print("#########################")
-
-
-
-
-# def main():
-#     print()
-#     print("Please input the number of the action that you would like to do.")
-#     print("1. View shopping list" '\n'
-#           "2. Add item to shopping cart" '\n'
-#           "3. Remove item from shopping cart" '\n'
-#           "4. View your shopping cart" '\n'
-#           "5. Go to checkout" '\n'
-#           "6. Exit")
-#     print()
-#     action = int(input("Please input the action number: "))
-#     print()
-#     while True:
-#         if action == 1:
-#             action1()
-#         elif action == 2:
-#             action2()
-#         elif action == 3:
-#             action3()
-#         elif action == 4:
-#             action4()
-#         elif action == 5:
-#             action5()
-#         elif action == 6:
-#             action6()
-#         else:
-#             print('Invalid number, please try again')
-#
-# total_price=[]
-# # user_shopping_list=[]
-# def action1():
-#     print("Do you want to display shopping list according to category, alphabetical order or in ascending price?")
-#     input("Please input C for category, A for alphabetical order and P for ascending price:")
-#
-# def action1_category():
-#     print("***DAIRY***")
-#     for key in dairy:
-#         print('%s - $%.2f' % (key, dairy[key]))
-#     print()
-#
-#     print("***PACKAGED GOODS***")
-#     for key in packaged_goods:
-#         print('%s - $%.2f' % (key, packaged_goods[key]))
-#     print()
-#
-#     print("***CANNED GOODS***")
-#     for key in canned_goods:
-#         print('%s - $%.2f' % (key, canned_goods[key]))
-#     print()
-#
-#     print("***CONDIMENTS/SAUCES***")
-#     for key in condiments_sauces:
-#         print('%s - $%.2f' % (key, condiments_sauces[key]))
-#     print()
-#
-#     print("***DRINKS AND BEVERAGES***")
-#     for key in drinks_and_beverages:
-#         print('%s - $%.2f' % (key, drinks_and_beverages[key]))
-#     print()
-#
-# def action2():
-#     action1_category()
-#     global qlist
-#     qlist=[]
-#     while True:
-#         num=input('Key the number of the item you would like to get [item (no.)]: ')
-#         global q
-#         q=int(input('Key the number of the items you would like: '))
-#         user_shopping_list.append('{} x {}'.format(q,num))
-#         total_price.append(q*shopping_menu.get(num))
-#         choice=input('Would you like to add more things to your cart?(y/n) ')
-#         if choice == 'y':
-#             continue
-#         else:
-#             main()
-#             break
-#
-# def action3():
-#     print(user_shopping_list)
-#     while True:
-#         print()
-#         print('Be warned that all quantities of the item would be removed')
-#         print()
-#         delete=int(input('Key in the number that you would like to remove'
-#                          '(1 being the first item in the list)  : '))
-#         user_shopping_list.remove(user_shopping_list[delete-1])
-#         total_price.remove(total_price[delete-1])
-#         choice=input('Would you like to delete more things from your cart?(y/n) ')
-#         if choice == 'y':
-#             print(user_shopping_list)
-#             continue
-#         else:
-#             main()
-#             break
-#
-#
-#
-# main()
-
